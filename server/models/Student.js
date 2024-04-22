9e899fce3795db876c9ef6ba10f4ede240ac1e21
CHUSUEYEON <rmffkfk226@naver.com>
2024-04-22 21:22:24 +0900

    feat: [BE] models/Tutor, Student.js 작성

---
+++ server/models/Student.js
@@ -0,0 +1,36 @@
+const Student = function (Sequelize, DataTypes) {
+    return Sequelize.define(
+        "Student",
+        {
+            stu_idx: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false,
+            },
+            id: {
+                type: DataTypes.STRING(20),
+                allowNull: false,
+            },
+            nickname: {
+                type: DataTypes.STRING(10),
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING(30),
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING(50),
+                allowNull: false,
+            },
+        },
+        {
+            tableName: "Student",
+            freezeTableName: true,
+            timestamps: true,
+        }
+    );
+};
+
+module.exports = Student;
