1cc19bcc7952b75bb91785d75b7cfc07c2f63766
leekihyeok <789rlgur@naver.com>
2024-04-24 01:21:53 +0900

    feat: [FE] Main페이지 구성 및 Scss file 생성, router 형식 변환 완료

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+// import Login from "./pages/Login";
+// import SignUp from "./pages/SignUp";
+// import PersonalLearning from "./pages/PersonalLearning";
+// import Quiz from "./pages/Quiz";
+import "./styles/index.scss";
 
 function App() {
     return (
-        <>
-            <h1></h1>
-        </>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            {/* <Route path="/login" element={<Login />} /> */}
+            {/* <Route path="/signup" element={<SignUp />} /> */}
+            {/* <Route path="/personal-learning" element={<PersonalLearning />} /> */}
+            {/* <Route path="/quiz" element={<Quiz />} /> */}
+        </Routes>
     );
 }
 
---
+++ client/src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+export default function Main() {
+    return (
+        <>
+            <Header />
+            <section></section>
+            <section></section>
+            <Footer />
+        </>
+    );
+}
