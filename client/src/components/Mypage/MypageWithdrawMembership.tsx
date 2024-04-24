b74cdd09ec10faff986d40ad4c36b7143a7f97fc
leekihyeok <789rlgur@naver.com>
2024-04-24 18:15:08 +0900

    feat: [FE] Mypage 컴포넌트 구성 및 메뉴 버튼 생성, Mypage 프로필 변경에서 닉네임, 비밀번호 수정 완료

---
+++ client/src/components/Mypage/MypageWithdrawMembership.tsx
@@ -0,0 +1,9 @@
+export default function MypageWithdrawMembership() {
+    return (
+        <>
+            <section>
+                <h1>MypageWithdrawMembership</h1>
+            </section>
+        </>
+    );
+}
---
+++ client/src/components/Mypage/StudentMypageLesson.tsx
@@ -0,0 +1,9 @@
+export default function StudentMypageLesson() {
+    return (
+        <>
+            <section>
+                <h1>StudentMypageLesson</h1>
+            </section>
+        </>
+    );
+}
