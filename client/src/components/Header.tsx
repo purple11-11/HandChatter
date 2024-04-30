c4c6f1ee50d5e12e21e2100d939be2eecbc5140e
leekihyeok <789rlgur@naver.com>
2024-04-30 14:37:37 +0900

    feat: [FE] Header 메뉴 axios 연결 및 토글 메뉴 작성

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Sidebar from "./Sidebar";
+import axios from "axios";
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [userInfo, setUserInfo] = useState<any>(null);
+
+    useEffect(() => {
+        getUser();
+    }, []);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
+    };
+
+    const handleLeftClick = () => {
+        window.location.href = "/mypage";
+    };
+
+    const handleRightClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        event.preventDefault();
+        setIsMenuOpen(true);
+        toggleMenu();
+    };
+
+    const getUser = async () => {
+        try {
+            const url = `${process.env.REACT_APP_API_SERVER}/api/userInfo`;
+            const res = await axios.get(url);
+            console.log(res.data.studentInfo[0]);
+            setUserInfo(res.data.studentInfo[0]);
+        } catch (error) {
+            console.error("사용자 정보를 불러오는 중 에러:", error);
+        }
     };
 
     return (
@@ -23,17 +50,26 @@ const Header = () => {
                         <li>
                             <Link to="/quiz">퀴즈</Link>
                         </li>
-                        <li>
-                            <Link to="/login">로그인</Link>
-                        </li>
-                        <li>
-                            <Link to="/signup/student">회원가입</Link>
-                        </li>
-                        <li>
-                            <Link to="/signup/mypage" onClick={toggleMenu}>
-                                -----님
-                            </Link>
-                        </li>
+                        {!userInfo ? (
+                            <>
+                                <li>
+                                    <Link to="/login">로그인</Link>
+                                </li>
+                                <li>
+                                    <Link to="/signup/student">회원가입</Link>
+                                </li>
+                            </>
+                        ) : (
+                            <li>
+                                <Link
+                                    to="/mypage"
+                                    onClick={handleLeftClick}
+                                    onContextMenu={handleRightClick}
+                                >
+                                    {userInfo.nickname} 님
+                                </Link>
+                            </li>
+                        )}
                     </ul>
                 </nav>
             </header>
