915d32270c94f75584b67fd8c1466ecb4e898072
leekihyeok <789rlgur@naver.com>
2024-05-01 17:15:48 +0900

    feat: [FE] zustant 적용(로그인, 헤더, 유저정보)

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,12 +3,17 @@ import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import axios from "axios";
+import { useInfoStore } from "../store/store"; // Importing the store
 
 const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [userInfo, setUserInfo] = useState<any>(null);
-    const [profileImgUrl, setProfileImgUrl] = useState<string>("");
     const navigate = useNavigate();
+
+    // Using Zustand store
+    const isLogin = useInfoStore((state) => state.isLogin);
+    const userInfo = useInfoStore((state) => state.userInfo);
+    const profileImgUrl = useInfoStore((state) => state.profileImgUrl);
+    const logout = useInfoStore((state) => state.logout);
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -28,57 +33,19 @@ const Header = () => {
         try {
             const url = `${process.env.REACT_APP_API_SERVER}/api/logout`;
             await axios.post(url);
-
-            localStorage.removeItem("isLoggedIn");
-            localStorage.removeItem("role");
-            setIsMenuOpen(false); // 로그아웃 후 메뉴 닫기
-            setUserInfo(null); // 로그아웃 후 사용자 정보 초기화
+            setIsMenuOpen(false);
+            logout();
         } catch (error) {
             console.error("로그아웃 오류:", error);
         }
     };
 
-    const getUser = async () => {
-        try {
-            console.log("++++");
-            const url = `${process.env.REACT_APP_API_SERVER}/api/userInfo`;
-            const res = await axios.get(url);
-            const role = localStorage.getItem("role");
-            console.log(role);
-
-            console.log(res.data);
-            if (role === "student") {
-                const newProfileImgUrl =
-                    process.env.REACT_APP_API_SERVER + res.data.studentInfo[0].profile_img;
-                console.log(res.data);
-                setProfileImgUrl(newProfileImgUrl);
-                setUserInfo(res.data.studentInfo[0]);
-                console.log("+++");
-            } else {
-                const newProfileImgUrl =
-                    process.env.REACT_APP_API_SERVER + res.data.tutorInfo[0].profile_img;
-                setProfileImgUrl(newProfileImgUrl);
-                setUserInfo(res.data.tutorInfo[0]);
-            }
-        } catch (error) {
-            setUserInfo(null);
-        }
-    };
-
-    const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
-    useEffect(() => {
-        console.log(isLoggedIn + "-------");
-        if (isLoggedIn) {
-            getUser();
-        }
-    }, [isLoggedIn]);
-
+    console.log(isLogin, userInfo, profileImgUrl);
     return (
         <>
             <header className="header">
                 <div className="container">
                     <div className="logo">
-                        {/* <img src={Logo} alt="" /> */}
                         <Link to="/">HandChatter</Link>
                     </div>
                     <nav className="menu">
@@ -89,7 +56,7 @@ const Header = () => {
                             <li>
                                 <Link to="/quiz">퀴즈</Link>
                             </li>
-                            {!userInfo ? (
+                            {!isLogin ? (
                                 <>
                                     <li>
                                         <Link to="/login">로그인</Link>
@@ -109,7 +76,7 @@ const Header = () => {
                                             <div className="profile-img">
                                                 <img src={profileImgUrl} alt="" />
                                             </div>
-                                            <div>{userInfo.nickname} 님</div>
+                                            <div>{userInfo?.nickname} 님</div>
                                         </div>
                                     </Link>
                                 </li>
