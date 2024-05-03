85cdab193fc5de13aeba90a07ed1ad313b4ef784
leekihyeok <789rlgur@naver.com>
2024-05-03 14:14:33 +0900

    feat: [FE] 비밀번호 수정

--- server/controller/Cmain.js
+++ server/controller/Cmain.js
@@ -571,7 +571,7 @@ exports.editStudentProfile = async (req, res) => {
         if (!id) return res.status(400).send("로그인을 해주세요.");
 
         const { nickname, password } = req.body;
-        if (!nickname || !password) res.status(400).send("빈칸을 입력해주세요.");
+        if (!nickname || !password) return res.status(400).send("빈칸을 입력해주세요.");
 
         const student = await Student.findOne({
             where: {
@@ -581,7 +581,7 @@ exports.editStudentProfile = async (req, res) => {
         if (student) {
             const checkPassword = comparePW(password, student.password);
             if (!checkPassword) {
-                res.status(400).send("유효한 비밀번호를 입력해주세요.");
+                return res.status(400).send("유효한 비밀번호를 입력해주세요.");
             } else {
                 await Student.update(
                     {
